271618192689a583f32cca20ae9dd62f8777216c
Tejas111 <tejaskumark11@gmail.com>
2018-07-08 17:02:24 +0530

    sessions

--- node-js/app.js
+++ node-js/app.js
@@ -9,6 +9,8 @@ var Dishes = require('./models/dishes');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dish');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var app = express();
 //for connection using mongdb
 const url = 'mongodb://localhost:27017/tejas';
@@ -26,7 +28,53 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser('12345-45678'));//secret key
+app.use(session({
+    name:'session-id',
+    secret:'12345-6789',
+    saveUninitialized:false,
+    resave:false,
+    store:new FileStore()
+}));
+function auth (req, res, next) {
+  console.log(req.session);
+  if(!req.session.user){
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      //setting up the cookie
+      req.session.user='admin';
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+else {
+    if (req.session.user === 'admin') {
+        next();
+    }
+    else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
